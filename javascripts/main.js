9fef47c5e714b85f7c58c2e59fbd38cdff03026a
Latasha Simmons <lsimmons1832@gmail.com>
2017-05-12 22:06:38 -0500

    assignment complete

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,33 @@
 var app = angular.module("Callan", []);
 
 app.controller("ItemCtrl", ($scope) => {
-	
-	
+	$scope.showListView = false;
+	$scope.link = "/images/Thoughts.png";
+
+	$scope.items = [
+	{
+		food: "Pie"
+	},
+	{
+		food: "Pizza"
+	},
+	{
+		food: "Donuts"
+	},
+	{
+		food: "Cookies"
+	},
+	{
+		food: "Ice Cream"
+	}
+	];
+
+	$scope.randomFood = $scope.items[Math.floor(Math.random() * $scope.items.length)];
+
+	$scope.showFav = () =>{
+		// let count = $scope.items.id;
+		$scope.showListView = true;
+		
+	};
 	
 });
